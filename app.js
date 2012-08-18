c1c97e4385f5ee55812aa94e3157fa91439664fe
Maiah Macariola <jjmmacariola@gmail.com>
2012-08-18 11:46:17 +0800

    integrating Connect's Session middleware

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿var flatiron = require('flatiron'),
+    app = flatiron.app,
+    connect = require('connect');
+
+var authenticate = function(req, res) {
+  console.log('Authenticating...');
+  res.emit('next');
+};
+
+app.use(flatiron.plugins.http, {
+  before: [
+    connect.favicon(),
+    connect.bodyParser(),
+    connect.cookieParser('rounds catspy'),
+    connect.session(),
+    authenticate
+  ]
+});
+
+app.router.get('/', function() {
+  this.res.writeHead(200, { "Content-Type": "text/plain" });
+  this.res.end("This app shows a basic Flatiron.js authentication.");
+});
+
+app.start(7000, function(err) {
+  console.log('Flatiron started on port 7000');
+});
