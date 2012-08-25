e2a68b635658773b49982efbc213291a6f1a6e39
Maiah Macariola <jjmmacariola@gmail.com>
2012-08-25 11:04:18 +0800

    shows basic login/logout

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 flatiron-auth
 =============
 
-Basic sample Flatiron.js authentication
+Basic sample Flatiron.js authentication. It showcases Flatiron's Union, Director, and Plates modules. It also shows integration w/ Connect's Session middleware.
--- app.js
+++ app.js
@@ -11,32 +11,19 @@ var users = {
 app.use(flatiron.plugins.http, {
   before: [
     connect.favicon(),
+    connect.cookieParser('catpsy speeds'),
     function(req, res) {
       if (req.originalUrl === undefined) {
         req.originalUrl = req.url;
       }
       res.emit('next');
     },
+    connect.session(),
     function(req, res) {
       console.log('Authenticating...');
-      console.log('REQUEST HEADERS:');
-      console.log(req.headers.cookie);
-      if (req.url !== '/login') {
-        //var errMsg = req.session.loginError ? '<i>' + req.session.loginError + '</i><br />' : '';
-        var html = '<!DOCTYPE html>' +
-                   '<html>' +
-                   '<body>' +
-                   //errMsg +
-                   '<form action="/login" method="POST">' +
-                   '<input type="text" name="username" placeholder="Username" /><br />' +
-                   '<input type="password" name="password" placeholder="Password" /><br />' +
-                   '<input type="submit" value="Login" />' +
-                   '</form>' +
-                   '</body>' +
-                   '<html>';
-
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(html);
+      console.dir(req.session);;
+      if (req.url !== '/login' && req.session.user === undefined) {
+        res.redirect('/login');
       } else {
         res.emit('next');
       }
@@ -45,22 +32,70 @@ app.use(flatiron.plugins.http, {
 });
 
 app.router.get('/', function() {
-  this.res.writeHead(200, { "Content-Type": "text/plain" });
-  this.res.end("This app shows a basic Flatiron.js authentication.");
+  var html = 'Hello <span id="user"></span>. This app shows a basic Flatiron.js authentication. ' + 
+             '<a href="/logout">Logout.</a>';
+  var data = { "user": this.req.session.user };
+
+  this.res.writeHead(200, { "Content-Type": "text/html" });
+  this.res.end(Plates.bind(html, data));
+});
+
+app.router.get('/login', function() {
+  if (this.req.session.user) {
+    this.res.redirect('/');
+  } else {
+    var loginError = this.req.session.loginError ? this.req.session.loginError : '';
+    var data = { "errMsg": loginError };
+    var html = '<!DOCTYPE html>' +
+               '<html>' +
+               '<body>' +
+               '<div id="errMsg"></div>' +
+               '<form action="/login" method="POST">' +
+               '<input type="text" name="username" placeholder="Username" /><br />' +
+               '<input type="password" name="password" placeholder="Password" /><br />' +
+               '<input type="submit" value="Login" />' +
+               '</form>' +
+               '</body>' +
+               '<html>';
+
+    this.res.writeHead(200, { "Content-Type": "text/html" });
+    this.res.end(Plates.bind(html, data));
+  }
 });
 
 app.router.post('/login', function() {
+  var res = this.res;
+
   var username = this.req.body.username;
   var userPass = users[username];
   if (userPass && userPass === this.req.body.password) {
-    console.log('Login correct!');
-    this.res.setHeader('Set-Cookie', ['user=' + username]);  
+    delete this.req.session['loginError'];
+    this.req.session.user = username;
   } else {
-    console.log('Login wrong!');
-    //this.req.session.loginError = 'Wrong username or password.';
+    delete this.req.session['user'];
+    this.req.session.loginError = 'Wrong username or password.';
   }
 
+  this.req.session.save(function(err) {
+    if (err) {
+      throw err
+    }
+    res.redirect('/');
+  });
+});
+
+app.router.get('/logout', function() {
+  this.req.session.destroy();
   this.res.redirect('/');
+});
+
+app.router.get('/hey', function() {
+  var html = 'Hello <span id="user"></span>. This is another secured page. ' + 
+             '<a href="/logout">Logout.</a>';
+  var data = { "user": this.req.session.user };
+
+  this.res.writeHead(200, { "Content-Type": "text/html" });
+  this.res.end(Plates.bind(html, data));
 });
 
 app.listen(7000, function(err) {
